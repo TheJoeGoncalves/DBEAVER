0ea645afe3e08b056e655887bbf02089e7384141
joegoncalves <jg4536a@american.edu>
2024-10-20 23:01:18 -0400

    Answer question two

--- JG.sql
+++ JG.sql
@@ -12,3 +12,26 @@ ON
 GROUP BY 
     f.origin;
     
+   
+   
+   
+   
+   SELECT 
+    origin, 
+    AVG(daily_flights) AS avg_flights_per_day
+FROM (
+    SELECT 
+        origin, 
+        year, 
+        month, 
+        day, 
+        COUNT(*) AS daily_flights
+    FROM 
+        flights
+    WHERE 
+        month = 1  -- January
+    GROUP BY 
+        origin, year, month, day
+) AS daily_flight_counts
+GROUP BY 
+    origin;
